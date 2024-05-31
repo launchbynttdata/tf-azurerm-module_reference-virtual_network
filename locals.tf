21f72664d078c8d99e7022c46b4ba435bc6728d0
Chris Taylor <chris11.taylor@nttdata.com>
2024-05-31 09:33:26 -0500

    Fix variable reference

--- locals.tf
+++ locals.tf
@@ -9,7 +9,7 @@ locals {
   virtual_network_name = module.resource_names["virtual_network"].standard
 
   transformed_route_tables = {
-    for route_table_alias, route_table_definition in var.var.route_tables :
+    for route_table_alias, route_table_definition in var.route_tables :
     route_table_alias => merge(route_table_definition, {
       resource_group_name = local.resource_group_name
       tags                = merge(local.default_tags, route_table_definition.extra_tags)
