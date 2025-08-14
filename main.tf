87f749c9ba1383a1a751598e753d2003020fa0b0
Raghuramakrishna <340229@nttdata.com>
2025-08-14 21:03:07 +0530

    update the files

--- main.tf
+++ main.tf
@@ -309,12 +309,15 @@ module "monitor_private_link_scope_private_endpoint" {
   depends_on = [module.resource_group, module.subnets, module.monitor_private_link_scope, module.monitor_private_link_scope_dns_zone]
 }
 
-resource "azurerm_private_dns_zone" "this" {
-  count = var.private_dns_zone_enabled && length(var.private_dns_zone_ids) == 0 ? 1 : 0
-
+resource "azurerm_private_dns_zone" "postgres" {
   name                = "privatelink.postgres.database.azure.com"
   resource_group_name = module.resource_group.name
   tags                = var.tags
+}
+
+output "postgres_private_dns_zone_id" {
+  value       = azurerm_private_dns_zone.postgres.id
+  description = "The ID of the Postgres private DNS zone."
 }
 
 locals {
